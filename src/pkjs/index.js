dd9b605d8f17aef1f7354f3aa0a18523078e502c
JS <r0blox1053@outlook.com>
2021-04-21 14:04:41 -0700

    Remove logs & add color corrections for non-color

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -2,6 +2,7 @@ require('pebblejs');
 var UI = require('pebblejs/ui');
 var Voice = require('pebblejs/ui/voice');
 var Motor = require('pebblejs/ui/vibe');
+var Feature = require('pebblejs/platform/feature');
 var Settings = require('pebblejs/settings');
 var Clay = require('./clay');
 var clayConfig = require('./config');
@@ -16,8 +17,6 @@ var messages = [];
 var selectedChannel;
 var inChatList = false;
 
-console.log("Configured with: " + token + ", " + isABot);
-
 Pebble.addEventListener('showConfiguration', function(e) {
 	Pebble.openURL(clay.generateUrl());
 });
@@ -27,7 +26,6 @@ Pebble.addEventListener('webviewclosed', function(e) {
 		return;
 	}
 	var dict = clay.getSettings(e.response);
-	console.log("Message to send:" + JSON.stringify(dict));
 	Settings.option(dict);
 });
 
@@ -37,35 +35,34 @@ var loadingCard = new UI.Card({
 	titleColor: 'orange'
 });
 
-if(!token){
-	loadingCard.title("Oops!");
-	loadingCard.subtitle("Enter your Discord token in the app settings!");
-	console.log("No token!");
+if (!token) {
+	loadingCard.title('Oops!');
+	loadingCard.subtitle('Enter your Discord token in the app settings!');
 }
 
 loadingCard.show();
 
 var serverMenu = new UI.Menu({
 	status: 'none',
-	backgroundColor: 'liberty',
-	textColor: 'white',
-	highlightBackgroundColor: 'indigo',
+	backgroundColor: Feature.color('liberty', 'white'),
+	textColor: 'black',
+	highlightBackgroundColor: Feature.color('indigo', 'black'),
 	highlightTextColor: 'white',
 	sections: [
 		{ title: 'Servers' },
 	]
 });
 
-if(!isABot){
+if (!isABot) {
 	serverMenu.section(0, { title: 'DMs' });
 	serverMenu.section(1, { title: 'Servers' });
 }
 
 var channelMenu = new UI.Menu({
 	status: 'none',
-	backgroundColor: 'liberty',
-	textColor: 'white',
-	highlightBackgroundColor: 'indigo',
+	backgroundColor: Feature.color('liberty', 'white'),
+	textColor: 'black',
+	highlightBackgroundColor: Feature.color('indigo', 'black'),
 	highlightTextColor: 'white',
 	sections: [{
 	  title: 'Channels',
@@ -78,13 +75,13 @@ var chatList = new UI.Card({
 	scrollable:true,
 	title: 'Loading...',
 	titleColor: 'orange',
-	subtitleColor: 'chrome yellow',
+	subtitleColor: Feature.color('chrome yellow', 'black'),
 	bodyColor: 'black',
 });
 
-serverMenu.on("select", function(selection) {
+serverMenu.on('select', function(selection) {
 	channels = [];
-	channelMenu.items(0, {title:"Loading..."});
+	channelMenu.items(0, { title: 'Loading...' });
 	var j = 0;
 	if(selection.sectionIndex || isABot)
 		for(var i = 0; i < servers[selection.itemIndex].channels.length; i++){
@@ -101,11 +98,11 @@ serverMenu.on("select", function(selection) {
 		for(var i = 0; i < dmChannels.length; i++){
 			channels.push(dmChannels[i]);
 			if(dmChannels[i].recipients.length > 1){
-				var members = "";
+				var members = '';
 				for(var m = 0; m < dmChannels[i].recipients.length; m++)
-					members += dmChannels[i].recipients[m].username + ", ";
+					members += dmChannels[i].recipients[m].username + ', ';
 				channelMenu.item(0, j, {
-					title: "Group with " + dmChannels[i].recipients[0].username,
+					title: 'Group with ' + dmChannels[i].recipients[0].username,
 					subtitle: members
 				});
 			}else
@@ -115,31 +112,27 @@ serverMenu.on("select", function(selection) {
 	channelMenu.show();
 });
 
-channelMenu.on("select", function(selection){
+channelMenu.on('select', function(selection){
 	messages = [];
-	console.log("Selected Channel!");
 	
 	selectedChannel = channels[selection.itemIndex].id;
 	
 	var request = new XMLHttpRequest();
 	
 	request.onload = function() {
-		console.log('Got response: ' + this.responseText);
 		messages = JSON.parse(this.responseText);
-		console.log(JSON.stringify(messages[0]));
-		var cardBody = "";
-		chatList.title(messages[0].author.username + ":");
+		var cardBody = '';
+		chatList.title(messages[0].author.username + ':');
 		chatList.subtitle(messages[0].content);
 		for(var i = 1; i < messages.length; i++)
-			cardBody += messages[i].author.username + ":\n" + messages[i].content + "\n";
+			cardBody += messages[i].author.username + ':\n' + messages[i].content + '\n';
 		if(cardBody.length > 433 - messages[0].author.username.length - messages[0].content.length);
-		chatList.body(cardBody.substring(0, 410 - messages[0].author.username.length - messages[0].content.length) + "...");
-		console.log("Chat list text length: " + (cardBody.substring(0, 410 - messages[0].author.username.length - messages[0].content.length).length +3));
+		chatList.body(cardBody.substring(0, 410 - messages[0].author.username.length - messages[0].content.length) + '...');
 	};
 
-	request.open("GET", "https://discordapp.com/api/channels/" + selectedChannel + "/messages");
+	request.open('GET', 'https://discordapp.com/api/channels/' + selectedChannel + '/messages');
 	if(isABot)
-		request.setRequestHeader('Authorization', "Bot " + token);
+		request.setRequestHeader('Authorization', 'Bot ' + token);
 	else
 		request.setRequestHeader('Authorization', token);
 	request.setRequestHeader('Content-Type', 'application/json');
@@ -158,14 +151,10 @@ chatList.on('click', 'select', function(input){
 		}
 	
 		var message = new XMLHttpRequest();
-		
-		message.onload = function() {
-			console.log('Got response: ' + this.responseText);
-		};
 
-		message.open("POST", "https://discordapp.com/api/channels/" + selectedChannel + "/messages");
+		message.open('POST', 'https://discordapp.com/api/channels/' + selectedChannel + '/messages');
 		if(isABot)
-			message.setRequestHeader('Authorization', "Bot " + token);
+			message.setRequestHeader('Authorization', 'Bot ' + token);
 		else
 			message.setRequestHeader('Authorization', token);
 		message.setRequestHeader('Content-Type', 'application/json');
@@ -182,74 +171,64 @@ chatList.on('click', 'back', function(input){
 	inChatList = false;
 });
 
-var ws = new WebSocket("wss://gateway.discord.gg/?encoding=json&v=7");
+var ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=7');
 
 ws.onopen = function () {
     console.log('Connected!');
 }
 
 ws.onmessage = function(event) {
-    console.log(event);
-    
     var data = JSON.parse(event.data);
     
-    if(data.t === "READY"){
-        console.log("Ready!");
+    if(data.t === 'READY'){
+        console.log('Ready!');
         serverMenu.show();
         loadingCard.hide();
         if(!isABot){
-            console.log(JSON.stringify(data.d.guilds));
             servers = data.d.guilds;
             dmChannels = data.d.private_channels;
-            serverMenu.item(0, 0, {title: "Messages", subtitle: "Direct messages"});
+            serverMenu.item(0, 0, {title: 'Messages', subtitle: 'Direct messages'});
             for(var i = 0; i < servers.length; i++){
                 serverMenu.item(1, i, {title: data.d.guilds[i].name});
             }
         }
     }
     
-    if(data.t === "MESSAGE_CREATE"){
+    if(data.t === 'MESSAGE_CREATE'){
         setTimeout(function(){
-            console.log("New Message! Currently in chat list: " + inChatList);
             if(data.d.channel_id == selectedChannel && inChatList){
                 Motor.vibrate('short');
                 messages.unshift(data.d);
-                var cardBody = "";
-                chatList.title(messages[0].author.username + ":");
+                var cardBody = '';
+                chatList.title(messages[0].author.username + ':');
                 chatList.subtitle(messages[0].content);
                 for(var i = 1; i < messages.length; i++)
-                    cardBody += messages[i].author.username + ":\n" + messages[i].content + "\n";
+                    cardBody += messages[i].author.username + ':\n' + messages[i].content + '\n';
                 if(cardBody.length > 433 - messages[0].author.username.length - messages[0].content.length);
-                chatList.body(cardBody.substring(0, 410 - messages[0].author.username.length - messages[0].content.length) + "...");
-                console.log("Length of chat card: " + (410 - messages[0].author.username.length - messages[0].content.length + 3));
+                chatList.body(cardBody.substring(0, 410 - messages[0].author.username.length - messages[0].content.length) + '...');
             }
         }, 500);
     }
     
-    if(data.t === "GUILD_CREATE" && isABot){
-        console.log("Got Server: " + data.d.name);
+    if(data.t === 'GUILD_CREATE' && isABot){
         serverMenu.item(0, servers.length, {title: data.d.name});
         servers.push(data.d);
     }
-    console.log(event.data);
-    console.log(JSON.stringify(event));
+
     if(data.op === 10){
-        console.log("Heartbeat: " + data.d.heartbeat_interval);
-        console.log("Sent Auth!");
         ws.send(JSON.stringify({
-            "op": 2,
-            "d": {
-                "token": token,
-                "properties": {
-                    "$browser": "pebble",
+            'op': 2,
+            'd': {
+                'token': token,
+                'properties': {
+                    '$browser': 'pebble',
                 },
-                "large_threshold": 50,
+                'large_threshold': 50,
             }
         }));
         
         setInterval(function(){
-            console.log("Sent heartbeat!");
-            ws.send(JSON.stringify({"op": 1, "d":null}));
+            ws.send(JSON.stringify({'op': 1, 'd':null}));
         }, data.d.heartbeat_interval - 500);
     }
 }
